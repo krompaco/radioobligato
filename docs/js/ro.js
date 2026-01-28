15277f10f962e33428416c04bd727cb6c19f0fa3
Johan Kronberg <johan.kronberg@krompaco.nu>
2026-01-28 09:59:59 +0100

    Update ro.js

--- docs/js/ro.js
+++ docs/js/ro.js
@@ -7,14 +7,6 @@ $(document).ready(function () {
 		var cont = '<a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#106;&#107;&#064;&#114;&#097;&#100;&#105;&#111;&#111;&#098;&#108;&#105;&#103;&#097;&#116;&#111;&#046;&#099;&#111;&#109;" title="E-mail Johan">' + $(this).html() + '</a>';
 		$(this).html(cont);
 	});
-	$('#profile-link').click(function () {
-		pageTracker._trackPageview('/profile-link');
-	});
-	$('.playlist h3 a').each(function () {
-		$(this).click(function () {
-			pageTracker._trackPageview('/' + $(this).attr("id"));
-		});
-	});
 	$('#order-link').each(function () {
 		var cont = '<a href="#">' + openText + '</a>';
 		$(this).html(cont);
@@ -30,19 +22,5 @@ $(document).ready(function () {
 			}
 			return false;
 		});
-	});
-	$('ol[data-spotify-uri]').each(function () {
-		var ol = $(this);
-		var li = ol.parent();
-		var iframe = $('<' + 'iframe src="https://embed.spotify.com/?uri=' + ol.data('spotify-uri') + '" width="300" height="380" frameborder="0" allowtransparency="true" scrolling="no"><' + '/iframe>');
-		$('<a class="show-embed" href="#' + li.attr('id') + '">Show Spotify Embed</a>')
-			.click(function (e) {
-				e.preventDefault();
-				var a = $(this);
-				a.hide();
-				ol.hide();
-				iframe.appendTo(li);
-			})
-			.appendTo(li);
 	});
 });
